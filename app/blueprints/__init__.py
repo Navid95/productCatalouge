05f806d1769296dde0a57872bc250bbe9726424e
navid.mhkh@gmail.com <navid.mhkh@gmail.com>
2024-03-12 21:31:58 +0330

    app.bluprints.__init__: BaseService class developed to act the service layer to include business logics

---
+++ app/blueprints/__init__.py
@@ -0,0 +1,15 @@
+from uuid import UUID
+
+from app.models import BaseModel
+
+
+class BaseService:
+    def __init__(self, model: BaseModel):
+        self.__model__ = model
+
+    def get_model_by_id(self, model_id: UUID):
+        model_object = self.__model__.get(model_id)
+        if not model_object:
+            return None
+        return model_object
+
