2ddec1015df6854b5cb245c7590c42069fad54f6
navid.mhkh@gmail.com <navid.mhkh@gmail.com>
2024-03-16 16:47:57 +0330

    BaseService.create_sub_model: developed

--- app/blueprints/__init__.py
+++ app/blueprints/__init__.py
@@ -25,7 +25,25 @@ class BaseService:
     def get_all_models(self, limit=10, page=1):
         return self.__model__.get_all(limit=limit, page=page)
 
-    def get_sub_model(self, model_id: UUID, sub_resource_key: str):
+    def get_sub_model(self, model_id: UUID, sub_model_key: str):
         model_object = self.__model__.get(model_id)
-        sub_model_list = getattr(model_object, sub_resource_key)
+        sub_model_list = getattr(model_object, sub_model_key)
         return sub_model_list
+
+    def create_sub_model(self, model_id: UUID, sub_resource_list: list | BaseModel, sub_resource_key: str,
+                         many: bool = False):
+        model = self.__model__.get(model_id)
+        if many:
+            sub_resources = list()
+            for sub_resource_instance in sub_resource_list:
+                sub_resource = sub_resource_instance.get(sub_resource_instance.id)
+                if sub_resource:
+                    sub_resources.append(sub_resource)
+            setattr(model, sub_resource_key, sub_resources)
+        else:
+            sub_resource = sub_resource_list.get(sub_resource_list.id)
+            if sub_resource:
+                setattr(model, sub_resource_key, sub_resource)
+
+        put_result = self.__model__.put(model)
+        return getattr(model, sub_resource_key)
