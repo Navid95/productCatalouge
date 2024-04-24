4c10d5975e46021489083b56fe125bb68ad3947a
navid.mhkh@gmail.com <navid.mhkh@gmail.com>
2024-04-24 22:10:15 +0330

    after request function enhanced to log to db

--- app/utilities/logging/api_log.py
+++ app/utilities/logging/api_log.py
@@ -5,8 +5,10 @@ from flask import g
 from flask import request
 from flask import Response
 
+from app.models.log import IncomingAPI
 from environ import API_LOGGER_NAME
 from environ import APP_LOGGER_NAME
+from app.extensions import db
 
 app_logger = logging.getLogger(APP_LOGGER_NAME)
 api_logger = logging.getLogger(API_LOGGER_NAME)
@@ -30,4 +32,17 @@ def log_api_call(response: Response):
     }
     api_logger.info(msg=message)
     app_logger.info(msg=message)
+
+    incoming_api = IncomingAPI(url=request.url,
+                               method=request.method,
+                               headers=str(request.headers),
+                               body=str(request.get_json(silent=True)) or request.get_data(),
+                               r_headers=str(response.headers),
+                               r_body=str(response.get_json(silent=True)) or response.get_data(),
+                               status_code=str(response.status_code),
+                               status=response.status,
+                               remote_address=request.remote_addr)
+    db.session.add(incoming_api)
+    db.session.commit()
+
     return response
