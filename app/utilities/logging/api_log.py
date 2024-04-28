7832bdc1de9a4d71662b8e0aa89dbda6ca298829
Navid Mehdinejad <navid.mhkh@gmail.com>
2024-04-28 21:11:56 +0330

    models.log.IncomingAPI: developed - api_log.log_api_call: enhanced to log to file and db

--- app/utilities/logging/api_log.py
+++ app/utilities/logging/api_log.py
@@ -1,33 +1,50 @@
 import logging
 from datetime import datetime
+from copy import deepcopy
 
 from flask import g
 from flask import request
 from flask import Response
 
+from app.models.log import IncomingAPI
 from environ import API_LOGGER_NAME
 from environ import APP_LOGGER_NAME
+from app.extensions import db
 
 app_logger = logging.getLogger(APP_LOGGER_NAME)
 api_logger = logging.getLogger(API_LOGGER_NAME)
 
 
 def get_request_time():
-    request_time = datetime.now().isoformat()
+    request_time = datetime.now()
     setattr(g, 'request_time', request_time)
 
 
 def log_api_call(response: Response):
-    response_time = datetime.now().isoformat()
+    response_time = datetime.now()
     request_time = getattr(g, 'request_time')
-    request_data = request.data
-    response_data = response.data
     message = {
-        "request_time": request_time,
-        "response_time": response_time,
-        "request_data": request_data,
-        "response_data": response_data
+        'url': request.url,
+        'method': request.method,
+        'status_code': str(response.status_code),
+        'status': response.status,
+        'headers': str(request.headers),
+        'body': str(request.get_json(silent=True)) or request.get_data(),
+        'r_body': str(response.get_json(silent=True)) or response.get_data(),
+        'r_headers': str(response.headers),
+        'request_time': request_time,
+        'response_time': response_time,
+        'remote_address': request.remote_addr
     }
+
+    file_message = deepcopy(message)
+    file_message['request_time'] = request_time.isoformat()
+    file_message['response_time'] = response_time.isoformat()
     api_logger.info(msg=message)
     app_logger.info(msg=message)
+
+    incoming_api = IncomingAPI(**message)
+    db.session.add(incoming_api)
+    db.session.commit()
+
     return response
